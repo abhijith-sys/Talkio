57119913712b0df243ad7a56c794426ed95f8a87
abhijith-sys <abhijithani007@gmail.com>
2024-10-06 00:39:37 +0530

    redirect back to plans

--- talkiyo/src/components/common/FailurePage.jsx
+++ talkiyo/src/components/common/FailurePage.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const FailurePage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Set a timeout of 20 seconds to navigate to another page
+        const timer = setTimeout(() => {
+            navigate('/plans'); // Replace '/thanks' with your desired route
+        }, 10000); // 20 seconds in milliseconds
+
+        // Cleanup the timeout when the component is unmounted
+        return () => clearTimeout(timer);
+    }, [navigate]);
     return (
         <div className="container">
             <div className="row justify-content-center">
